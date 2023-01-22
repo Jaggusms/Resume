ea9ebb68ac4bfac15eda395fc9f74f662a06d989
Jagadeesh <sanniboinajagadeesh@gmail.com>
2023-01-23 00:00:22 +0530

    welome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
-import base64
-
+import base64,os
 PAGE_TITLE = "Jagadeesh Digital Resume"
 PAGE_ICON = "source/pageicon.png"
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON,layout="wide")
@@ -62,14 +61,14 @@ def img_to_bytes(img_path):
 def img_to_html(img_path):
   return "<img src='data:image/png;base64,{}' class='img-fluid'>  ".format(img_to_bytes(img_path))
 social_meadia={
-    "Linkedin":"https://www.linkedin.com/in/jagadeesh-sanniboina-a8096816b/",
-    "github":"https://github.com/Jaggusms",
-    "kaggle":"https://www.kaggle.com/jagadeeshsanniboina",
-    "medium": "https://medium.com/@jagadeeshmk",
-    "stackoverflow":"https://stackoverflow.com/users/9774886/jagadeesh-sanniboina?tab=profile",
-    "Twitter":"https://twitter.com/SanniboinaJ?t=Mw1069Dj1N5G0-kLGmCMKw",
-    "HackerRank": "https://www.hackerrank.com/sanniboinajagad1",
-    "HackerEarth":"https://www.hackerearth.com/@jagadeesh220"
+    "source/linkdin.png":"https://www.linkedin.com/in/jagadeesh-sanniboina-a8096816b/",
+    "source/github.png":"https://github.com/Jaggusms",
+    "source/kaggle.png":"https://www.kaggle.com/jagadeeshsanniboina",
+    "source/medium.png": "https://medium.com/@jagadeeshmk",
+    "source/stackoverflow.png":"https://stackoverflow.com/users/9774886/jagadeesh-sanniboina?tab=profile",
+    "source/twitter.png":"https://twitter.com/SanniboinaJ?t=Mw1069Dj1N5G0-kLGmCMKw",
+    "source/hacker_rank.png": "https://www.hackerrank.com/sanniboinajagad1",
+    "source/hacker_earth.png":"https://www.hackerearth.com/@jagadeesh220"
 }
 
 #col1, col2 = st.columns([3, 1],gap='small')
@@ -80,14 +79,43 @@ with col2:
 with col1:
     st.title("JAGADEESH SANNIBOINA")
     st.markdown(':heart_eyes: :wave: <p style=" color:Blue; ">B-Tech 2017-2021</p>', unsafe_allow_html=True)
-    st.write('''I am a Machine Learning Engineer who insights into Deep Learning, Neural Networks, Computer Vision
-    and Natural Language Processing. I am extending myself towards Could, Data Engineering and Science
-    to archive a full stack Machine Learner. I am Multitasking and Problem Solver.''')
+    st.markdown('''<p style=" color:black; " >
+                I am a Machine Learning Engineer who insights into Deep Learning, Neural Networks, Computer Vision and Natural Language Processing.
+                I am extending myself towards Could, Data Engineering and Science to archive a full stack Machine Learner. 
+                I am Multitasking and Problem Solver.</p>''', unsafe_allow_html=True)
     #cols=st.columns(len(social_meadia),gap="small")
-cols = st.columns(len(social_meadia),gap="small")
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+@st.cache(allow_output_mutation=True)
+def get_img_with_href(local_img_path, target_url):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}">
+            <img src="data:image/{img_format};base64,{bin_str}" width="25"/>
+        </a>'''
+    return html_code
+cols = st.columns(4,gap="small")
 for idx,(platform,link) in enumerate(social_meadia.items()):
-    cols[idx].write(f"[{platform}]({link})")
-
+    #cols[idx%4].write(f"[{platform}]({link})")
+    #cols[idx%4].markdown(f"[![linkdin]({platform})]({link})")
+    # st.markdown('''
+    # <a href="https://docs.streamlit.io>
+    #     <img src="data:image/gif;base64,{source/linkdin.png}" alt="cat gif"/>
+    # </a>''',
+    # unsafe_allow_html=True)
+    # st.markdown('''
+    # <a href="https://docs.streamlit.io">
+    #     <img src="source\\linkdin.png" />
+    # </a>''',unsafe_allow_html=True)
+    
+    gif_html = get_img_with_href(platform, link)
+    cols[idx%4].markdown(gif_html, unsafe_allow_html=True)
+    #cols[idx%4].markdown("[![](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
+    #cols[idx%4].markdown("[![linkdin](source\\linkdin.png)](https://www.linkedin.com/in/jagadeesh-sanniboina-a8096816b/)")
 # st.write('\n')
 # st.markdown('<h3 style=" color:black; ">Experience & Qulifications</h3>', unsafe_allow_html=True)
 # with open('html/img.html')as f:
