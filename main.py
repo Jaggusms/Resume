1cc0d47d725a24f3b03faec8a944772bf9bd66e5
Jagadeesh <sanniboinajagadeesh@gmail.com>
2023-01-23 17:54:12 +0530

    welome

--- main.py
+++ main.py
@@ -4,49 +4,75 @@ import base64,os
 PAGE_TITLE = "Jagadeesh Digital Resume"
 PAGE_ICON = "source/pageicon.png"
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON,layout="wide")
-st.markdown("""
-<style>
-#MainMenu {visibility:hidden}
-header {visibility: hidden}
-footer {visibility: hidden}
-.css-15zrgzn {display: none}
-.css-eczf16 {display: none}
-.css-jn99sy {display: none}
-button[title="View fullscreen"]{visibility: hidden;}
-</style>
-""",unsafe_allow_html=True)
+st.get_option("theme.primaryColor")
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
+    st.markdown(f"""<style>
     .stApp {{
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
         background-attachment: fixed;
         background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('source/charcoal.png')  
+    }}</style>""",unsafe_allow_html=True)
+add_bg_from_local('source/charcoal.png') 
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+@st.cache(allow_output_mutation=True)
+def get_img_with_href(local_img_path, target_url):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}">
+            <img align="left" src="data:image/{img_format};base64,{bin_str}" width="25"/>
+        </a>'''
+    return html_code 
+def get_img(local_img_path,message):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+            <p ><img align="left" src="data:image/{img_format};base64,{bin_str}" width="20"/> {message}</p>'''
+    return html_code 
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
-        <style>
-               .css-18e3th9 {
-                    padding-top: 0rem;
-                    padding-bottom: 10rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-               .css-1d391kg {
-                    padding-top: 3.5rem;
-                    padding-right: 1rem;
-                    padding-bottom: 3.5rem;
-                    padding-left: 1rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color:#336B6E ;">
+  <a class="navbar-brand" href="https://github.com/Jaggusms" target="_self">jagadeesh sanniboina</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/" target="_self">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#education">Education</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#work-experience">Work Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Portfolio">Portfolio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Skills">Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Projects">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#social-media">Social Media</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
 # st.markdown(img_to_html('source/ProfilePic.png'), unsafe_allow_html=True)
 # with open('html/img.html')as f:
 #  st.markdown(f"<html>{f.read()}</html>", unsafe_allow_html = True)
@@ -57,10 +83,10 @@ with open("css/style.css") as f:
 
 
 
-def img_to_bytes(img_path):
-  return base64.b64encode(Path(img_path).read_bytes()).decode()
-def img_to_html(img_path):
-  return "<img src='data:image/png;base64,{}' class='img-fluid'>  ".format(img_to_bytes(img_path))
+# def img_to_bytes(img_path):
+#   return base64.b64encode(Path(img_path).read_bytes()).decode()
+# def img_to_html(img_path):
+#   return "<img src='data:image/png;base64,{}' class='img-fluid'>  ".format(img_to_bytes(img_path))
 social_meadia={
     "source/linkdin.png":"https://www.linkedin.com/in/jagadeesh-sanniboina-a8096816b/",
     "source/github_logo.png":"https://github.com/Jaggusms",
@@ -75,34 +101,48 @@ social_meadia={
 }
 
 #col1, col2 = st.columns([3, 1],gap='small')
-col1,col2 = st.columns([3,1],gap="small")
+col1,col2 ,col3= st.columns([2,1,0.5],gap="medium")
 with col2:
     #st.markdown(img_to_html('source/ProfilePic.png'), unsafe_allow_html=True)
-    st.image('source/ProfilePic.png', caption=None, width=150)
+    st.image('source/my-image.png', caption=None, width=250)
 with col1:
-    st.title("JAGADEESH SANNIBOINA")
-    st.markdown(':heart_eyes: :wave: <p style="; ">B-Tech 2017-2021</p>', unsafe_allow_html=True)
-    st.markdown('''<p style="  " >
-                I am a Machine Learning Engineer who insights into Deep Learning, Neural Networks, Computer Vision and Natural Language Processing.
+    st.markdown("<p style='font-family:sans-serif; font-size: 20px;'>Hello, I'm</p>", unsafe_allow_html=True)
+    st.markdown('<p style="font-family:sans-serif; font-size: 42px;"><i>JAGADEESH SANNIBOINA<i></p>', unsafe_allow_html=True)
+    st.write('Machine Learning Engineer | Data Scientist')
+    #st.markdown('''<style>[data-testid=column]:nth-of-type(1) [data-testid=stVerticalBlock]{gap: 1rem;}</style>''', unsafe_allow_html=True)
+    st. write("--")
+    gif_html = get_img("source/mail1.png","sanniboinajagadeesh@gmail.com")
+    st.markdown(gif_html, unsafe_allow_html=True)
+    gif_html = get_img("source/contact.png","8500060896")
+    st.markdown(gif_html, unsafe_allow_html=True)
+    gif_html = get_img("source/location.png","Nellore,AP.")
+    st.markdown(gif_html, unsafe_allow_html=True)
+cols = st.columns([3 if i==12 else 0.5 for i in range(20) ],gap="large")
+with cols[0]:
+    gif_html = get_img_with_href("source/linkdin.png", social_meadia["source/linkdin.png"])
+    cols[0].markdown(gif_html, unsafe_allow_html=True)
+    gif_html = get_img_with_href("source/insta.png", social_meadia["source/insta.png"])
+    cols[1].markdown(gif_html, unsafe_allow_html=True)
+    gif_html = get_img_with_href("source/facebook.png", social_meadia["source/facebook.png"])
+    cols[2].markdown(gif_html, unsafe_allow_html=True)
+    with open("source/Jagadeesh_ML_Resume.pdf", "rb") as pdf_file:
+        PDFbyte = pdf_file.read()
+        cols[12].download_button(label=" 📄 Download CV",data=PDFbyte,file_name="souce/Jagadeesh_ML_Resume.pdf",mime="application/octet-stream")
+            
+            
+            
+cols=st.columns([0.25,1,2],gap="small")
+cols[1].image("source/ml.png",width=200)
+cols[2].markdown("<p style='font-family:sans-serif; font-size: 25px;'><b>Bio:<b> </p>", unsafe_allow_html=True)
+cols[2].markdown(''':arrow_right: I am a **Machine Learning Engineer** who insights into _Deep Learning, Neural Networks, Computer Vision and Natural Language Processing_.
                 I am extending myself towards Could, Data Engineering and Science to archive a full stack Machine Learner. 
-                I am Multitasking and Problem Solver.</p>''', unsafe_allow_html=True)
+                I am Multitasking and good Problem Solver.''')
     #cols=st.columns(len(social_meadia),gap="small")
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-@st.cache(allow_output_mutation=True)
-def get_img_with_href(local_img_path, target_url):
-    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
-    bin_str = get_base64_of_bin_file(local_img_path)
-    html_code = f'''
-        <a href="{target_url}">
-            <img align="left" src="data:image/{img_format};base64,{bin_str}" width="25"/>
-        </a>'''
-    return html_code
-cols = st.columns(5,gap="large")
-for idx,(platform,link) in enumerate(social_meadia.items()):
+
+#cols = st.columns(5,gap="large")
+#for idx,(platform,link) in enumerate(social_meadia.items()):
+    # gif_html = get_img_with_href(platform, link)
+    # cols[idx%5].markdown(gif_html, unsafe_allow_html=True)
     #cols[idx%4].write(f"[{platform}]({link})")
     #cols[idx%4].markdown(f"[![linkdin]({platform})]({link})")
     # st.markdown('''
@@ -115,8 +155,7 @@ for idx,(platform,link) in enumerate(social_meadia.items()):
     #     <img src="source\\linkdin.png" />
     # </a>''',unsafe_allow_html=True)
     
-    gif_html = get_img_with_href(platform, link)
-    cols[idx%5].markdown(gif_html, unsafe_allow_html=True)
+
     #cols[idx%4].markdown("[![](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
     #cols[idx%4].markdown("[![linkdin](source\\linkdin.png)](https://www.linkedin.com/in/jagadeesh-sanniboina-a8096816b/)")
 # st.write('\n')
@@ -124,18 +163,6 @@ for idx,(platform,link) in enumerate(social_meadia.items()):
 # with open('html/img.html')as f:
 #  st.markdown(f"<html>{f.read()}</html>", unsafe_allow_html = True)
 
-# st.text(
-#     """
-# -  7 Years expereince extracting actionable insights from data
-# - ✔️ Strong hands on experience and knowledge in Python and Excel
-# - ✔️ Good understanding of statistical principles and their respective applications
-# - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
-# """
-# )
-
-# path = "source/ProfilePic.png"
-# image = Image.open(path)
-# st.image(image, width = 150)
 
 
 
