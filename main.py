5fc347e8325d179b1312b196fb6c90149cbb1abe
jagadeesh <sanniboinajagadeesh@gmail.com>
2023-04-09 05:25:03 -0700

    resume

--- main.py
+++ main.py
@@ -104,6 +104,7 @@ with tab1s[0]:
         gihub = get_img_with_href("source/github_logo.png", social_meadia["source/github_logo.png"],25)
         kagle=get_img_with_href("source/kaggle.png", social_meadia["source/kaggle.png"],25)
         st.markdown(f"<p> {linkdin} {insta} {gihub} {kagle}</p>", unsafe_allow_html=True)
+        
     with col2:
         #st.markdown(img_to_html('source/ProfilePic.png'), unsafe_allow_html=True)
         st.image('source/ProfilePic.png', caption=None, width=155)
