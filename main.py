62320da81a48ab7af483ebc171cecb741a885ebe
Jagadeesh <38516306+Jaggusms@users.noreply.github.com>
2023-05-01 15:14:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ with tab1s[0]:
         
     with col2:
         #st.markdown(img_to_html('source/ProfilePic.png'), unsafe_allow_html=True)
-        st.image('source/ProfilePic.png', caption=None, width=155)
+        st.image('source/ProfilePic.png', caption=None, width=175)
         with open("source/Jagadeesh_ML_Resume.pdf", "rb") as pdf_file:
             PDFbyte = pdf_file.read()
             st.download_button(label="📄 Download CV",data=PDFbyte,file_name="souce/Jagadeesh_ML_Resume.pdf",mime="application/octet-stream")
