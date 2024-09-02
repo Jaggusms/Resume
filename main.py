d0ba9d762bc202ab00c7260bc2f25181c86486ac
Jagadeesh <38516306+Jaggusms@users.noreply.github.com>
2024-09-02 10:12:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ with tab1s[0]:
     with col2:
         #st.markdown(img_to_html('source/ProfilePic.png'), unsafe_allow_html=True)
         st.image('source/ProfilePic.png', caption=None, width=155)
-        with open("source/JAGADEESH SANNIBOINA.pdf", "rb") as pdf_file:
+        with open("source/JAGADEESH_SANNIBOINA.pdf", "rb") as pdf_file:
             PDFbyte = pdf_file.read()
             st.download_button(label="📄 Download CV",data=PDFbyte,file_name="JAGADEESH_SANNIBOINA.pdf",mime="application/octet-stream")
        
